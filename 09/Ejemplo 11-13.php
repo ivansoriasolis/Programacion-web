5373785e4dcdc84a8181a462fd93a46953f27df9
ivansoriasolis <soria.solis.ivan@googlemail.com>
2020-07-28 19:04:48 -0500

    comunicación asíncrona

--- 09/Ejemplo 11-13.php
+++ 09/Ejemplo 11-13.php
@@ -4,6 +4,10 @@ numeros.push("Uno")
 numeros.push("Dos")
 numeros.push("Tres")
 
+letras = ["a", "b", "c"]
+
+
+
 for (j = 0; j < numeros.length; ++j)
     document.write("Elemento " + j + " = " + numeros[j] + "<br>")
 </script>
